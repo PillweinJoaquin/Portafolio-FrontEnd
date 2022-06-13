79485b5d9a293391beebda3a21d876490c336eba
PillweinJoaquin <100744806+PillweinJoaquin@users.noreply.github.com>
2022-06-13 14:26:01 -0300

    Mejoras

---
+++ Portafolio-FronEnd-Bootstrap/objetos.js
@@ -0,0 +1,28 @@
+// saludo
+let saludo = "Hola" ;
+console.log(saludo +" Programadores! "+ " 🤗");
+
+function mostrarInformacion(){
+    document.getElementById('desaparecer').style.display= 'block';
+}
+function cerrarInformacion(){
+    document.getElementById('desaparecer').style.display = 'none';
+}
+
+
+
+// mostrat interes
+
+function mostrar(){
+    document.getElementById('interes').style.display= 'block';
+}
+
+function cerrar(){
+    document.getElementById('interes').style.display = 'none';
+}
+
+
+// fecha de año
+const año= new Date();
+let year = año.getFullYear();
+document.getElementById("anio").innerHTML = year;
